f64264b51f29d66df3dde397b4e454d95a0fb1a0
Your Name <you@example.com>
2022-10-19 21:15:35 +0800

    add optimizer support.

--- run_cls.sh
+++ run_cls.sh
@@ -1,8 +1,11 @@
 CUDA_VISIBLE_DEVICES='2' PYTHONPATH=/home/penghy/jittor-dev/jittor/python:/home/penghy/PointCloudLib python train_cls.py \
 --model pointnext \
 --data_dir /home/penghy/data/ModelNet40Ply2048/ \
---mode test \
+--mode train \
 --dataset_type ModelNet40-h5 \
---pretrained_path ./pm.pth \
 --configs ./pointnext-pre.yaml \
---num_points 1024
+--num_points 1024 \
+--lr 0.001 \
+--optimizer adamw \
+--weight_decay 0.05 
+# --pretrained_path ./pm.pth \
