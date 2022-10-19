a2f3f5b1992fc32b63e9fbdd85dcdcd8e300285d
Exusial <bilibili39@163.com>
2022-10-19 21:15:18 +0800

    debug forward.

--- run_cls.sh
+++ run_cls.sh
@@ -1,9 +1,12 @@
 CUDA_VISIBLE_DEVICES='2' python train_cls.py \
 --model pointnext \
 --data_dir /home/penghy/PointNeXt/data/ModelNet40Ply2048/ \
---mode test \
+--mode train \
 --dataset_type ModelNet40-h5 \
---pretrained_path /home/penghy/PointNeXt/pm.pth \
 --configs /home/penghy/PointNeXt/cfgs/modelnet40ply2048/pointnext-pre.yaml \
---num_points 1024
+--num_points 1024 \
+--lr 0.001 \
+--optimizer adamw \
+--weight_decay 0.05 
+# --pretrained_path /home/penghy/PointNeXt/pm.pth \
 
