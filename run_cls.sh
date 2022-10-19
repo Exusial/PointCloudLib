0561794ca1e51235aa4c05e225b2f7cb6788412c
Your Name <you@example.com>
2022-10-19 17:03:48 +0800

    start train.

--- run_cls.sh
+++ run_cls.sh
@@ -1,1 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 python train_cls.py --model pointnet
+CUDA_VISIBLE_DEVICES='2' PYTHONPATH=/home/penghy/jittor-dev/jittor/python:/home/penghy/PointCloudLib python train_cls.py \
+--model pointnext \
+--data_dir /home/penghy/data/ModelNet40Ply2048/ \
+--mode test \
+--dataset_type ModelNet40-h5 \
+--pretrained_path ./pm.pth \
+--configs ./pointnext-pre.yaml \
+--num_points 1024
